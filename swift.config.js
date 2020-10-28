841c9ec42e11796beb24f113f2652155dc9808e2
Bimaa Ghafara <bima@icube.us>
2020-10-28 15:44:46 +0700

    clean swift-config

--- swift.config.js
+++ swift.config.js
@@ -93,21 +93,6 @@ const sentry = {
     },
 };
 
-/* List Of CMS Pages: [url-1, url-2, ..., url-n] */
-const cmsPages = ['about-us', 'aw-reward-points', 'privacy-policy-cookie-restriction-mode'];
-
-/* Contact Us */
-// identifiers for cmsBlocks in contact page
-const cmsContactIdentifiers = 'weltpixel_contact_page';
-
-/* Dashboard */
-// identifiers for cmsBlocks in contact page
-const cmsSocialMediaLinkIdentifiers = 'pwa_socmed_links';
-
-/* Social media link */
-// social media link in dashboard
-const enableSocialMediaLink = true;
-
 /* Loader */
 const loaderImage = '/assets/img/loader.svg';
 
@@ -132,35 +117,6 @@ const features = {
         enabled: false,
         app_id: '', // if enabled add fb app id here. e.g. 3080154482073095
     },
-    // masuk module -> pindah jika module sudah siap
-    productAvailableToCart: {
-        SimpleProduct: true,
-        ConfigurableProduct: true,
-        VirtualProduct: true,
-        GroupedProduct: false,
-        BundleProduct: true,
-        DownloadableProduct: false,
-    },
-    imageSize: {
-        product: {
-            width: 240,
-            height: 300,
-        },
-        homeSlider: {
-            mobile: {
-                width: 960,
-                height: 1120,
-            },
-            desktop: {
-                width: 1800,
-                height: 750,
-            },
-        },
-        category: {
-            width: 960,
-            height: 577,
-        },
-    },
     vesMenu: {
         enabled: true,
     },
@@ -182,206 +138,8 @@ const features = {
     },
 };
 
-const modules = {
-    about: {
-        enabled: true,
-        path: '/about-us',
-    },
-    blog: {
-        enabled: true,
-        path: '/blog',
-        link: {
-            detail: {
-                href: '/blog/[id]',
-                as: '/blog/',
-            },
-            category: {
-                href: '/blog/category/[id]',
-                as: '/blog/category/',
-            },
-            default: {
-                href: '/blog',
-            },
-        },
-        featuredImage: true,
-    },
-    brands: {
-        enabled: true,
-        path: '/brands',
-    },
-    catalog: {
-        enabled: true,
-        productListing: {
-            pageSize: 10,
-            drawerFilterOnDesktop: {
-                enabled: false, // used if need to desktop view on large screen
-            },
-            label: {
-                enabled: true,
-                new: {
-                    enabled: true,
-                },
-                sale: {
-                    enabled: true,
-                },
-            },
-            configurableOptions: {
-                enabled: false,
-            },
-            rating: {
-                enabled: true,
-            },
-        },
-        pdp: {
-            popupDetailImage: {
-                enabled: true,
-            },
-        },
-    },
-    confirmpayment: {
-        enabled: true,
-        path: '/confirmpayment',
-    },
-    checkout: {
-        enabled: true,
-        path: '/checkout',
-        ipayUrl: {
-            local: 'https://swiftpwa-be.testingnow.me/ipayredirect/ipayredirect/?orderId=',
-            dev: 'https://swiftpwa-be.testingnow.me/ipayredirect/ipayredirect/?orderId=',
-            stage: 'https://swiftpwa-be.testingnow.me/ipayredirect/ipayredirect/?orderId=',
-            prod: 'https://b2cdemo.getswift.asia/ipayredirect/ipayredirect/?orderId=',
-        },
-        snapUrl: {
-            dev: 'https://app.sandbox.midtrans.com/snap/snap.js',
-            prod: 'https://app.midtrans.com/snap/snap.js',
-        },
-        pickupStore: {
-            enabled: true,
-        },
-        extraFee: {
-            enabled: true,
-        },
-    },
-    cart: {
-        enabled: true,
-        path: '/checkout/cart',
-    },
-    customer: {
-        enabled: true,
-        path: '/customer',
-        plugin: {
-            address: {
-                splitCity: true,
-            },
-        },
-    },
-    contact: {
-        enabled: true,
-        path: '/contact',
-    },
-    forgotpassword: {
-        enabled: true,
-        path: '/customer/account/forgotpassword',
-    },
-    rewardpoint: {
-        enabled: true,
-        path: '/aw_rewardpoints/info',
-    },
-    rma: {
-        enabled: true,
-        path: '/rma/customer',
-    },
-    storecredit: {
-        enabled: true,
-        path: '/customer/account/storecredit',
-    },
-    storeLocator: {
-        enabled: true,
-        path: '/storelocator',
-    },
-    giftcard: {
-        enabled: true,
-        path: '/awgiftcard/card',
-    },
-    login: {
-        enabled: true,
-        path: '/customer/account/login',
-    },
-    notification: {
-        enabled: true,
-        path: 'inboxnotification/notification',
-    },
-    register: {
-        enabled: true,
-        path: '/customer/account/create',
-    },
-    trackingorder: {
-        enabled: true,
-        path: '/sales/order/track',
-    },
-    thanks: {
-        enabled: true,
-        path: '/checkout/onepage/success',
-    },
-    home: {
-        enabled: true,
-        featuresProduct: {
-            enable: true,
-            url_key: 'homepage-featured-products',
-        },
-        categoryList: {
-            enable: true,
-            url_key: 'homepage-featured-categories',
-            imageSize: {
-                mobile: {
-                    width: 960,
-                    height: 577,
-                },
-                desktop: {
-                    width: 404,
-                    height: 465,
-                },
-            },
-        },
-        bannerSlider: {
-            enable: true,
-        },
-    },
-    promo: {
-        enabled: true,
-    },
-    order: {
-        enabled: true,
-        path: '/sales/order',
-    },
-    wishlist: {
-        enabled: true,
-        path: '/wishlist',
-    },
-    maintenance: {
-        enabled: true,
-        path: '/maintenance',
-    },
-    error: {
-        enabled: true,
-    },
-};
-
 const nossrCache = [
-    '/aw_rewardpoints/info',
-    '/sales/order/history',
-    '/customer/account/profile',
-    '/customer/account/address',
-    '/awgiftcard/card',
-    '/customer/account/storecredit',
-    '/inboxnotification/notification',
-    '/customer/setting',
-    '/rma/customer',
-    '/confirmpayment',
-    '/checkout',
-    '/checkout/cart',
     '/graphql',
-    '/checkout/onepage/success',
 ];
 
 const debuging = {
@@ -403,18 +161,13 @@ module.exports = {
     nameToken,
     expiredToken,
     expiredDefault,
-    cmsPages,
     loaderImage,
-    cmsContactIdentifiers,
-    cmsSocialMediaLinkIdentifiers,
     custDataNameCookie,
     nameCheckoutCookie,
     nameGlobalCookie,
-    enableSocialMediaLink,
     features,
     nossrCache,
     recaptcha,
-    modules,
     installMessage,
     appName,
     localResolverKey,
