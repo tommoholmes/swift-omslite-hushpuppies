a64c5d51884fdaab0bf0493ff995fde81d2c68e0
Bimaa Ghafara <bima@icube.us>
2020-11-05 15:07:06 +0700

    remove teslogin, implement logic in login module

--- core/modules/login/pages/default/components/index.js
+++ core/modules/login/pages/default/components/index.js
@@ -1,16 +1,26 @@
-/* eslint-disable arrow-body-style */
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const Login = () => {
+const Login = (props) => {
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        handleSubmit,
+    } = props;
+
     return (
         <div className="login-container">
             <form>
                 <div className="row center-xs start-sm">
                     <div className="col-xs-12 col-sm-12">
                         <TextField
-                            name="username"
+                            name="email"
                             label="Email"
-                            placeholder="john.doe@gmail.com"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="col-xs-12 col-sm-12">
@@ -19,7 +29,17 @@ const Login = () => {
                             label="Password"
                             placeholder="******"
                             type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
+                    </div>
+                    <div>
+                        <Button
+                            onClick={handleSubmit}
+                            variant="contained"
+                        >
+                            Login
+                        </Button>
                     </div>
                 </div>
             </form>
