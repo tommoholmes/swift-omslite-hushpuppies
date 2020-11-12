d569f54859da8003eb1a2cb1c2604efd6f2c46bb
Bimaa Ghafara <bima@icube.us>
2020-11-12 17:22:11 +0700

    onprogress TablePagination

--- core/modules/commons/Table/index.js
+++ core/modules/commons/Table/index.js
@@ -6,16 +6,33 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
+import TablePaginationActions from './components/TablePaginationActions';
 
 const CustomTable = (props) => {
     const {
         columns,
         rows,
     } = props;
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    // methods
+    const handleChangePage = (event, newPage) => {
+        console.log(newPage);
+        setPage(newPage);
+    };
+    const handleChangeRowsPerPage = (event) => {
+        console.log(event.target.value);
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     React.useEffect(() => {
-    }, []);
+        console.log({ page });
+    }, [page]);
 
     const renderTableHeader = () => {
         return (
@@ -53,11 +70,35 @@ const CustomTable = (props) => {
         );
     };
 
+    const renderTableFooter = () => {
+        return (
+            <TableFooter>
+                <TableRow>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25, 50]}
+                        colSpan={3}
+                        count={24}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        SelectProps={{
+                            inputProps: { 'aria-label': 'rows per page' },
+                            native: true,
+                        }}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                        ActionsComponent={TablePaginationActions}
+                    />
+                </TableRow>
+            </TableFooter>
+        );
+    };
+
     return (
         <TableContainer component={Paper}>
             <Table>
                 {renderTableHeader()}
                 {renderTableBody()}
+                {renderTableFooter()}
             </Table>
         </TableContainer>
     );
