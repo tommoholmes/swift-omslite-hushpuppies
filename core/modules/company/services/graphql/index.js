299315827096b2476b782afe46c5d89657f7d5f7
Bimaa Ghafara <bima@icube.us>
2020-11-13 22:48:36 +0700

    update service: getCompanyList, getCompanyByCode, createCompany, updateCompany, deleteCompany,

--- core/modules/company/services/graphql/index.js
+++ core/modules/company/services/graphql/index.js
@@ -1,14 +1,38 @@
-import { useLazyQuery } from '@apollo/client';
+import { useQuery, useLazyQuery, useMutation } from '@apollo/client';
 import * as Schema from './schema';
 
-export const getCompanyList = (variables) => useLazyQuery(Schema.getCompanyList, {
-    variables,
-    context: {
-        request: 'internal',
-    },
+const context = {
+    context: { request: 'internal' },
+};
+
+const fetchPolicy = {
     fetchPolicy: 'cache-and-network',
+};
+
+export const getCompanyList = (variables) => useLazyQuery(Schema.getCompanyList, {
+    variables, ...context, ...fetchPolicy,
+});
+
+export const getCompanyByCode = (variables) => useQuery(Schema.getCompanyByCode, {
+    variables, ...context, ...fetchPolicy,
+});
+
+export const createCompany = (variables) => useMutation(Schema.createCompany, {
+    variables, ...context,
+});
+
+export const updateCompany = (variables) => useMutation(Schema.updateCompany, {
+    variables, ...context,
+});
+
+export const deleteCompany = (variables) => useMutation(Schema.deleteCompany, {
+    variables, ...context,
 });
 
 export default {
     getCompanyList,
+    getCompanyByCode,
+    createCompany,
+    updateCompany,
+    deleteCompany,
 };
--- core/modules/company/services/graphql/schema.js
+++ core/modules/company/services/graphql/schema.js
@@ -1,26 +1,98 @@
 import { gql } from '@apollo/client';
 
 export const getCompanyList = gql`
-query getCompanyList(
-    $pageSize: Int!,
-    $currentPage: Int!,
-) {
-    getCompanyList(pageSize: $pageSize, currentPage: $currentPage){
-        items {
-            company_id
-            company_code
-            company_name
-        }
-        total_count
-        page_info {
-            page_size
-            current_page
-            total_pages
+    query getCompanyList(
+        $pageSize: Int!,
+        $currentPage: Int!,
+    ){
+        getCompanyList(
+            pageSize: $pageSize,
+            currentPage: $currentPage
+        ){
+            items {
+                company_id
+                company_code
+                company_name
+            }
+            total_count
+            page_info {
+                page_size
+                current_page
+                total_pages
+            }
         }
     }
-  }
+`;
+
+export const getCompanyByCode = gql`
+    query getCompanyByCode(
+        $code: String!,
+    ){
+        getCompanyByCode(
+            code: $code
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export const createCompany = gql`
+    mutation createCompany(
+        $company_code: String!,
+        $company_name: String!,
+    ){
+        createCompany(
+            input: {
+                company_code: $company_code,
+                company_name: $company_name
+            }
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export const updateCompany = gql`
+    mutation updateCompany(
+        $id: Int!,
+        $company_code: String!,
+        $company_name: String!,
+    ){
+        updateCompany(
+            id: $id,
+            input: {
+                company_code: $company_code,
+                company_name: $company_name
+            }
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export const deleteCompany = gql`
+    mutation deleteCompany (
+        $id: Int!
+    ){
+        deleteCompany(
+            id: $id
+        )
+    }
 `;
 
 export default {
     getCompanyList,
+    getCompanyByCode,
+    createCompany,
+    updateCompany,
+    deleteCompany,
 };
