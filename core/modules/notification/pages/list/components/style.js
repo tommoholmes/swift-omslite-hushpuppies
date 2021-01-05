7caf295a7ac8453a8f6beaf81f47b25d427ab608
Vinsensius Niko <vinsensius.niko@sirclo.com>
2021-01-05 17:08:06 +0700

    26495143 - fixing filter module notification

--- core/modules/notification/pages/list/components/style.js
+++ core/modules/notification/pages/list/components/style.js
@@ -1,7 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const colorText = '#536777';
 
+const useStyles = makeStyles(() => ({
+    fieldInput: {
+        border: '1px solid',
+        borderColor: colorText,
+        height: 56,
+        borderRadius: 4,
+        '& .MuiInputBase-input': {
+            padding: '18.5px 14px',
+        },
+    },
 }));
 
 export default useStyles;
