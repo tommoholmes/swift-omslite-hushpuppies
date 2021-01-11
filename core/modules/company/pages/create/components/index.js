df2f1d962d9d565b0387a94104846ad4b47c4820
Vinsensius Niko <vinsensius.niko@sirclo.com>
2021-01-12 00:35:23 +0700

    26574398 - responsive company module

--- core/modules/company/pages/create/components/index.js
+++ core/modules/company/pages/create/components/index.js
@@ -2,8 +2,10 @@
 import React from 'react';
 import TextField from '@common_textfield';
 import Button from '@common_button';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Paper from '@material-ui/core/Paper';
 import { useRouter } from 'next/router';
+import clsx from 'clsx';
 import useStyles from './style';
 
 const CompanyCreateContent = (props) => {
@@ -15,39 +17,62 @@ const CompanyCreateContent = (props) => {
 
     return (
         <>
-            <h2 className={classes.title}>Create Company</h2>
+            <Button
+                className={classes.btnBack}
+                onClick={() => router.push('/oms/company')}
+                variant="contained"
+                style={{ marginRight: 16 }}
+            >
+                <ChevronLeftIcon style={{
+                    fontSize: 30,
+                    position: 'absolute',
+                    left: '50%',
+                    top: '50%',
+                    transform: 'translate(-50%, -50%)',
+                }}
+                />
+            </Button>
+            <h2 className={classes.titleTop}>Create Company</h2>
             <Paper className={classes.container}>
-                <div className={classes.formField}>
-                    <TextField
-                        variant="outlined"
-                        name="code"
-                        label="Company Code"
-                        value={formik.values.code}
-                        onChange={formik.handleChange}
-                        error={!!(formik.touched.code && formik.errors.code)}
-                        helperText={(formik.touched.code && formik.errors.code) || ''}
-                    />
+                <div className={classes.content}>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={clsx(classes.label, classes.labelRequired)}>Company Code</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="code"
+                            value={formik.values.code}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.code && formik.errors.code)}
+                            helperText={(formik.touched.code && formik.errors.code) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={clsx(classes.label, classes.labelRequired)}>Company Name</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
                 </div>
-                <div className={classes.formField}>
-                    <TextField
-                        variant="outlined"
-                        name="name"
-                        label="Company Name"
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                        error={!!(formik.touched.name && formik.errors.name)}
-                        helperText={(formik.touched.name && formik.errors.name) || ''}
-                    />
-                </div>
-                <div className={classes.formField}>
+                <div className={classes.formFieldButton}>
                     <Button
-                        onClick={() => router.push('/oms/company')}
-                        variant="contained"
-                        style={{ marginRight: 16 }}
-                    >
-                        Back
-                    </Button>
-                    <Button
+                        className={classes.btn}
                         onClick={formik.handleSubmit}
                         variant="contained"
                     >
