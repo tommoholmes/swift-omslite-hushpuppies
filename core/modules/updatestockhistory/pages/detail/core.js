a3a9e300edfecf85947ffa77489b6682bfb50888
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-03-29 22:08:57 +0700

    26574426 - add pages Marketplace/update stock history

---
+++ core/modules/updatestockhistory/pages/detail/core.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Layout from '@layout';
+import { useFormik } from 'formik';
+import { useRouter } from 'next/router';
+import gqlService from '../../services/graphql';
+
+const ContentWrapper = (props) => {
+    const {
+        data,
+        Content,
+    } = props;
+    const company = data.getCompanyById;
+
+    const formik = useFormik({
+        initialValues: {
+            code: company.company_code,
+            name: company.company_name,
+        },
+    });
+
+    const contentProps = {
+        formik,
+    };
+
+    return (
+        <Content {...contentProps} />
+    );
+};
+
+const Core = (props) => {
+    const router = useRouter();
+    const { loading, data } = gqlService.getCompanyById({
+        id: router && router.query && Number(router.query.id),
+    });
+
+    if (loading) {
+        return (
+            <Layout>Loading...</Layout>
+        );
+    }
+
+    if (!data) {
+        return (
+            <Layout>Data not found!</Layout>
+        );
+    }
+
+    return (
+        <Layout>
+            <ContentWrapper data={data} {...props} />
+        </Layout>
+    );
+};
+
+export default Core;
