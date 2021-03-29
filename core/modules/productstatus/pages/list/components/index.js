78784b93d6843192b5cc1cfc618f7dc2b65a8a29
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-03-29 22:05:58 +0700

    26574426 - add pages Marketplace/product status

---
+++ core/modules/productstatus/pages/list/components/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable object-curly-newline */
+import React from 'react';
+import Table from '@common_table';
+import Header from './Header';
+
+const ProductStatusListContent = (props) => {
+    const { data, loading, getCompanyList } = props;
+    const companyList = (data && data.getCompanyList && data.getCompanyList.items) || [];
+    const companyTotal = (data && data.getCompanyList && data.getCompanyList.total_count) || 0;
+
+    const columns = [
+        { field: 'company_id', headerName: 'ID' },
+        { field: 'company_code', headerName: 'SKU' },
+        { field: 'company_code', headerName: 'Marketplace Code' },
+        { field: 'company_name', headerName: 'Marketplace Store ID' },
+        { field: 'company_name', headerName: 'Marketplace Status' },
+        { field: 'company_id', headerName: 'Status' },
+        { field: 'company_id', headerName: 'Message' },
+        { field: 'company_id', headerName: 'Update At' },
+    ];
+
+    const rows = companyList.map((company) => ({
+        ...company,
+        id: company.company_id,
+    }));
+
+    // if (!data || loading) {
+    //     return (
+    //         <div>Loading . . .</div>
+    //     );
+    // }
+
+    return (
+        <>
+            <Header />
+            <Table
+                rows={rows}
+                getRows={getCompanyList}
+                loading={loading}
+                columns={columns}
+                count={companyTotal}
+            />
+        </>
+    );
+};
+
+export default ProductStatusListContent;
