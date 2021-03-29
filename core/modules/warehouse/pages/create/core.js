ebd57c60b385438103d5d4f9ed6bdf1709d2716b
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-03-29 22:01:46 +0700

    26574426 - add pages Marketplace/warehouse

---
+++ core/modules/warehouse/pages/create/core.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Layout from '@layout';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+// import { useRouter } from 'next/router';
+// import gqlService from '../../services/graphql';
+
+const Core = (props) => {
+    const {
+        Content,
+    } = props;
+    // const router = useRouter();
+    // const [createCompany] = gqlService.createCompany();
+
+    // const handleSubmit = ({
+    //     code,
+    //     name
+    // }) => {
+    //     const variables = {
+    //         company_code: code,
+    //         company_name: name
+    //     };
+    //     window.backdropLoader(true);
+    //     createCompany({
+    //         variables,
+    //     }).then(() => {
+    //         window.backdropLoader(false);
+    //         window.toastMessage({
+    //             open: true,
+    //             text: 'Success create new company!',
+    //             variant: 'success',
+    //         });
+    //         setTimeout(() => router.push('/marketplace/warehouse'), 250);
+    //     }).catch((e) => {
+    //         window.backdropLoader(false);
+    //         window.toastMessage({
+    //             open: true,
+    //             text: e.message,
+    //             variant: 'error',
+    //         });
+    //     });
+    // };
+
+    const formik = useFormik({
+        initialValues: {
+            code: '',
+            name: '',
+        },
+        validationSchema: Yup.object().shape({
+            code: Yup.string().required('Required!'),
+            name: Yup.string().required('Required!'),
+        }),
+        // onSubmit: (values) => {
+        // handleSubmit(values);
+        // },
+    });
+
+    const contentProps = {
+        formik,
+    };
+
+    return (
+        <Layout>
+            <Content {...contentProps} />
+        </Layout>
+    );
+};
+
+export default Core;
