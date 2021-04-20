70beee2e5a9fdde2f6e38b1e6ff423914a27ab07
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-04-20 15:26:03 +0700

    26574422 - add page location price upload

---
+++ core/modules/locationpriceupload/pages/import/core.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import Layout from '@layout';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import gqlService from '../../services/graphql';
+
+const Core = (props) => {
+    const {
+        Content,
+    } = props;
+    const [uploadPriceLocation] = gqlService.uploadPriceLocation();
+    const [downloadList, downloadListRes] = gqlService.downloadSampleCsv({ type: 'price_location' });
+
+    useEffect(() => {
+        downloadList();
+    }, []);
+
+    const urlDownload = downloadListRes && downloadListRes.data && downloadListRes.data.downloadSampleCsv;
+
+    const handleSubmit = ({
+        binary,
+    }) => {
+        const variables = {
+            binary,
+        };
+        window.backdropLoader(true);
+        uploadPriceLocation({
+            variables,
+        }).then(() => {
+            window.backdropLoader(false);
+            window.toastMessage({
+                open: true,
+                text: 'Success Upload Price Location',
+                variant: 'success',
+            });
+        }).catch((e) => {
+            window.backdropLoader(false);
+            window.toastMessage({
+                open: true,
+                text: e.message,
+                variant: 'error',
+            });
+        });
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            binary: '',
+        },
+        validationSchema: Yup.object().shape({
+            binary: Yup.string().required('Required!'),
+        }),
+        onSubmit: (values) => {
+            handleSubmit(values);
+        },
+    });
+
+    const handleDropFile = (files) => {
+        const fileName = files[0].file.name;
+        const { baseCode } = files[0];
+        formik.setFieldValue('filename', fileName);
+        formik.setFieldValue('binary', baseCode);
+    };
+
+    const contentProps = {
+        formik,
+        urlDownload,
+        handleDropFile,
+    };
+
+    return (
+        <Layout>
+            <Content {...contentProps} />
+        </Layout>
+    );
+};
+
+export default Core;
