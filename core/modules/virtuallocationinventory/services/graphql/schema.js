4092dc71b5475155d6dd11163cb6af829e6d0c58
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-04-20 15:19:40 +0700

    26574422 - add page virtual location inventory

---
+++ core/modules/virtuallocationinventory/services/graphql/schema.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+export const getCompanyList = gql`
+    query getCompanyList(
+        $pageSize: Int!,
+        $currentPage: Int!,
+    ){
+        getCompanyList(
+            pageSize: $pageSize,
+            currentPage: $currentPage
+        ){
+            items {
+                company_id
+                company_code
+                company_name
+            }
+            total_count
+            page_info {
+                page_size
+                current_page
+                total_pages
+            }
+        }
+    }
+`;
+
+export const getCompanyById = gql`
+    query getCompanyById(
+        $id: Int!,
+    ){
+        getCompanyById(
+            id: $id
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export const createCompany = gql`
+    mutation createCompany(
+        $company_code: String!,
+        $company_name: String!,
+    ){
+        createCompany(
+            input: {
+                company_code: $company_code,
+                company_name: $company_name
+            }
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export const updateCompany = gql`
+    mutation updateCompany(
+        $id: Int!,
+        $company_code: String!,
+        $company_name: String!,
+    ){
+        updateCompany(
+            id: $id,
+            input: {
+                company_code: $company_code,
+                company_name: $company_name
+            }
+        ){
+            company_code
+            company_id
+            company_name
+            is_new_product
+        }
+    }
+`;
+
+export default {
+    getCompanyList,
+    getCompanyById,
+    createCompany,
+    updateCompany,
+};
