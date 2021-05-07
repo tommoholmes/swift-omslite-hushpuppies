9ff24f3b66633ebf8c2063ca3257d7cd3308e808
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-05-07 15:33:53 +0700

    26574422 - add menu request vendor

---
+++ core/modules/requestvendor/pages/edit/components/index.js
@@ -0,0 +1,250 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import TextField from '@common_textfield';
+import Button from '@common_button';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Paper from '@material-ui/core/Paper';
+import { useRouter } from 'next/router';
+import clsx from 'clsx';
+import useStyles from './style';
+
+const RequestVendorEditContent = (props) => {
+    const {
+        formik,
+    } = props;
+    const classes = useStyles();
+    const router = useRouter();
+
+    return (
+        <>
+            <Button
+                className={classes.btnBack}
+                onClick={() => router.push('/vendorportal/requestvendor')}
+                variant="contained"
+                style={{ marginRight: 16 }}
+            >
+                <ChevronLeftIcon style={{
+                    fontSize: 30,
+                    position: 'absolute',
+                    left: '50%',
+                    top: '50%',
+                    transform: 'translate(-50%, -50%)',
+                }}
+                />
+            </Button>
+            <h2 className={classes.titleTop}>
+                Edit Vendor
+                {formik.values.name}
+            </h2>
+            <Paper className={classes.container}>
+                <div className={classes.content}>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={clsx(classes.label, classes.labelRequired)}>Firstname</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="code"
+                            value={formik.values.code}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.code && formik.errors.code)}
+                            helperText={(formik.touched.code && formik.errors.code) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Lastname</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Email</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Address</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Country</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Province</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>City</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Telephone</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Company Name</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Company Code</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="code"
+                            value={formik.values.code}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.code && formik.errors.code)}
+                            helperText={(formik.touched.code && formik.errors.code) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.divLabel}>
+                            <span className={classes.label}>Status</span>
+                        </div>
+                        <TextField
+                            className={classes.fieldRoot}
+                            variant="outlined"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={!!(formik.touched.name && formik.errors.name)}
+                            helperText={(formik.touched.name && formik.errors.name) || ''}
+                            InputProps={{
+                                className: classes.fieldInput,
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className={classes.formFieldButton}>
+                    <Button
+                        className={classes.btn}
+                        // onClick={formik.handleSubmit}
+                        variant="contained"
+                    >
+                        Approve
+                    </Button>
+                    <Button
+                        className={classes.btn}
+                        // onClick={formik.handleSubmit}
+                        variant="contained"
+                    >
+                        Not Approved
+                    </Button>
+                </div>
+            </Paper>
+        </>
+    );
+};
+
+export default RequestVendorEditContent;
