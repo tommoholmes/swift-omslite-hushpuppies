4827017df04426bab25dc89a352755e8b3f32c1f
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-09-07 18:51:04 +0700

    26464596 - fixing core modules virtualstock

--- core/modules/virtualstock/pages/edit/index.js
+++ core/modules/virtualstock/pages/edit/index.js
@@ -1,7 +1,7 @@
 // import { withTranslation } from '@i18n';
 import { withApollo } from '@lib_apollo';
-import Content from './components';
-import Core from './core';
+import Content from '@modules/virtualstock/pages/edit/components';
+import Core from '@modules/virtualstock/pages/edit/core';
 
 const Page = (props) => (
     <Core
