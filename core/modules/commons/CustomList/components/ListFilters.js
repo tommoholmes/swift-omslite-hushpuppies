0c5539d096f580d90a03d2aa8c7b6df027b368bc
alulsirclo <fazlur.rahman@sirclo.com>
2021-10-04 13:13:38 +0700

    26980990 - create common list mobile

---
+++ core/modules/commons/CustomList/components/ListFilters.js
@@ -0,0 +1,100 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import TextField from '@common_textfield';
+import Button from '@common_button';
+import PropTypes from 'prop-types';
+import classnames from 'clsx';
+
+const defaultFilterComponent = ({ filterValue, setFilterValue, disabled }) => (
+    <TextField
+        variant="outlined"
+        size="small"
+        value={filterValue}
+        onChange={(e) => setFilterValue(e.target.value)}
+        disabled={disabled}
+    />
+);
+
+const ListFilters = (props) => {
+    const { initialFilters, setParentFilters, handleReset = () => {} } = props;
+
+    // state
+    const [filters, setFilters] = React.useState(initialFilters);
+    const emptyFiltersField = filters && !filters.length;
+
+    // methods
+    const getFilterValueByField = (field) => {
+        const index = filters.findIndex((filter) => filter.name === field.name);
+        return index >= 0 ? filters[index].value : '';
+    };
+    const setFilterValueByField = (field, value) => {
+        const index = filters.findIndex((filter) => filter.name === field.name);
+        if (index >= 0) {
+            setFilters(filters.map((filter) => ({
+                ...filter,
+                ...(filter.name === field.name && { ...field, value }),
+            })));
+        } else {
+            setFilters([...filters, { ...field, value }]);
+        }
+    };
+
+    return (
+        <div style={{ padding: 12 }}>
+            {emptyFiltersField && (
+                <div style={{ padding: 12 }}>Filter fields is empty.</div>
+            )}
+            {filters.map((field, i) => (
+                field.hidden ? null
+                    : (
+                        <div className={classnames('col-filter', field.class)} key={i} style={{ padding: 12, display: 'inline-block' }}>
+                            <div>
+                                {field.label}
+                            </div>
+                            {(field.component || defaultFilterComponent)({
+                                get filterValue() { return getFilterValueByField(field); },
+                                setFilterValue: (value) => setFilterValueByField(field, value),
+                                disabled: field.disabled,
+                            })}
+                        </div>
+                    )
+            ))}
+            <div style={{ padding: 12 }}>
+                <Button
+                    buttonType="primary-rounded"
+                    onClick={() => {
+                        // only set filters which have value
+                        if (!emptyFiltersField) setParentFilters(filters);
+                    }}
+                >
+                    Apply Filters
+                </Button>
+                <Button
+                    buttonType="link"
+                    onClick={() => {
+                        if (!emptyFiltersField) {
+                            const resetedFilters = filters.map((filter) => ({ ...filter, value: '' }));
+                            setFilters(resetedFilters);
+                            setParentFilters([]);
+                            handleReset();
+                        }
+                    }}
+                >
+                    Clear Filters
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+ListFilters.propTypes = {
+    initialFilters: PropTypes.array,
+};
+
+ListFilters.defaultProps = {
+    initialFilters: [],
+};
+
+export default ListFilters;
---
+++ core/modules/commons/CustomList/style.js
@@ -0,0 +1,112 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const colorText = '#435179';
+const colorTextGray = '#B1BCDB';
+const borderGray = '#E5E9F1';
+const colorPurple = '#BE1F93';
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        padding: '12px 15px',
+        background: '#ffffff',
+        borderRadius: 16,
+        marginBottom: 8,
+        '& hr': {
+            margin: '12px -15px',
+            background: borderGray,
+            border: 0,
+            height: 1,
+        },
+    },
+    tableToolbar: {
+        '& .top-buttons-wrapper': {
+            padding: '16px',
+            textAlign: 'right',
+            [theme.breakpoints.down('xs')]: {
+                textAlign: 'unset',
+            },
+        },
+        '& .records-found': {
+            padding: '12px 0',
+            float: 'left',
+            marginLeft: '12px',
+        },
+        '& .top-item': {
+            display: 'inline-block',
+            marginRight: '12px',
+            '& .MuiButton-text': {
+                border: '1px solid',
+                borderColor: colorText,
+                color: colorText,
+                textTransform: 'capitalize',
+            },
+        },
+        '& .top-item.records-found': {
+            [theme.breakpoints.down('xs')]: {
+                display: 'block',
+                float: 'none',
+            },
+        },
+        '& .boxColumn': {
+            display: 'inline-block',
+            [theme.breakpoints.down('xs')]: {
+                display: 'block',
+            },
+        },
+        '& .MuiCollapse-wrapperInner .col-filter': {
+            [theme.breakpoints.down('xs')]: {
+                width: '100%',
+            },
+            '& .MuiAutocomplete-root': {
+                [theme.breakpoints.down('xs')]: {
+                    width: '100% !important',
+                },
+            },
+        },
+        '& .MuiCollapse-wrapperInner .col-filter .MuiTextField-root': {
+            [theme.breakpoints.down('xs')]: {
+                width: '100%',
+            },
+        },
+    },
+    btn: {
+        borderRadius: 20,
+        background: 'unset',
+        boxShadow: 'none',
+        textTransform: 'capitalize',
+        color: colorText,
+        border: '1px solid',
+        borderColor: colorText,
+        '&:hover': {
+            background: 'unset',
+            boxShadow: 'none',
+        },
+        '&.filter': {
+            borderColor: colorPurple,
+            color: 'white',
+            background: colorPurple,
+        },
+    },
+    gridList: {
+        display: 'grid',
+    },
+    titleList: {
+        color: colorTextGray,
+        fontSize: 10,
+        margin: 0,
+    },
+    bodyList: {
+        color: colorText,
+        fontSize: 12,
+        margin: 0,
+    },
+    loading: {
+        display: 'flex',
+        color: colorText,
+        fontWeight: 600,
+        justifyContent: 'center',
+        padding: '20px 0',
+    },
+}));
+
+export default useStyles;
