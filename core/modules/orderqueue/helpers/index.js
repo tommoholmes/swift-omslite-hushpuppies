a82e877d7cff02894c26aab337a2b6e77315f848
Abdurozzaq Nurul Hadi <abdurozzaq.nurul@mail.sirclo.com>
2021-10-21 11:19:16 +0700

    27021275 - Edit Order Queue Filter Field

---
+++ core/modules/orderqueue/helpers/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+export const optionsStatus = [
+    {
+        name: 'Allocating',
+        idValue: 'allocating',
+    },
+    {
+        name: 'Failed',
+        idValue: 'failed',
+    },
+    {
+        name: 'New',
+        idValue: 'new',
+    },
+    {
+        name: 'Processing',
+        idValue: 'processing',
+    },
+];
