003118b8ab8390947a0df08c05d8e0e6361d0893
arfanjadulhaq <arfan.jadulhaq@mail.sirclo.com>
2021-11-09 13:16:43 +0700

    update page updatestockbyfilehistory

--- core/modules/updatestockbyfilehistory/services/graphql/index.js
+++ core/modules/updatestockbyfilehistory/services/graphql/index.js
@@ -1,14 +1,21 @@
-import { useMutation } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import * as Schema from '@modules/updatestockbyfilehistory/services/graphql/schema';
 
 const context = {
     context: { request: 'internal' },
 };
 
-export const createCompany = (variables) => useMutation(Schema.createCompany, {
-    variables, ...context,
+export const getFileHistoryTypes = (variables) => useLazyQuery(Schema.getFileHistoryTypes, {
+    variables,
+    ...context,
+});
+
+export const getUpdateStockByFileHistoryList = (variables) => useLazyQuery(Schema.getUpdateStockByFileHistoryList, {
+    variables,
+    ...context,
 });
 
 export default {
-    createCompany,
+    getFileHistoryTypes,
+    getUpdateStockByFileHistoryList,
 };
