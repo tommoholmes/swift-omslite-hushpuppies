b6c69cc2fa3b91b803d29fc150539c937b7600df
arfanjadulhaq <arfan.jadulhaq@mail.sirclo.com>
2021-11-11 14:53:32 +0700

    add page stock summary list & export stock summary

--- core/modules/theme/layout/index.js
+++ core/modules/theme/layout/index.js
@@ -86,22 +86,46 @@ const Layout = (props) => {
                 //     url: '/sales/orderqueue',
                 // },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_allocating', key: 'orderqueue_allocating', label: 'Allocating', url: '/sales/allocating', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_allocating',
+                    key: 'orderqueue_allocating',
+                    label: 'Allocating',
+                    url: '/sales/allocating',
+                    notInAcl: true,
                 },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_failed', key: 'orderqueue_failed', label: 'Failed', url: '/sales/failed', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_failed',
+                    key: 'orderqueue_failed',
+                    label: 'Failed',
+                    url: '/sales/failed',
+                    notInAcl: true,
                 },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_order_processing', key: 'orderqueue_order_processing', label: 'Order Processing', url: '/sales/order_processing', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_order_processing',
+                    key: 'orderqueue_order_processing',
+                    label: 'Order Processing',
+                    url: '/sales/order_processing',
+                    notInAcl: true,
                 },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_shipment_processing', key: 'orderqueue_shipment_processing', label: 'Shipment Processing', url: '/sales/shipment_processing', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_shipment_processing',
+                    key: 'orderqueue_shipment_processing',
+                    label: 'Shipment Processing',
+                    url: '/sales/shipment_processing',
+                    notInAcl: true,
                 },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_complete', key: 'orderqueue_complete', label: 'Complete', url: '/sales/complete', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_complete',
+                    key: 'orderqueue_complete',
+                    label: 'Complete',
+                    url: '/sales/complete',
+                    notInAcl: true,
                 },
                 {
-                    aclCode: 'oms_lite_sales_order_queue_canceled', key: 'orderqueue_canceled', label: 'Canceled', url: '/sales/canceled', notInAcl: true,
+                    aclCode: 'oms_lite_sales_order_queue_canceled',
+                    key: 'orderqueue_canceled',
+                    label: 'Canceled',
+                    url: '/sales/canceled',
+                    notInAcl: true,
                 },
                 // { aclCode: 'orderreallocation', label: 'Order Reallocation', url: '/sales/orderreallocation' },
             ],
@@ -239,6 +263,12 @@ const Layout = (props) => {
                     key: 'stocktransfer',
                     label: 'Stock Transfer',
                     url: '/cataloginventory/stocktransfer',
+                },
+                {
+                    aclCode: 'stock_summary',
+                    key: 'stocksummary',
+                    label: 'Stock Summary',
+                    url: '/cataloginventory/stocksummary',
                 },
                 {
                     aclCode: 'inventory_adjustment_dashboard',
@@ -587,7 +617,7 @@ const Layout = (props) => {
     };
 
     useEffect(() => {
-        const activeMenuFirstChild = mappedMenuList.find((e) => (e.url === (router && router.asPath) || e.url === (router && router.pathname)));
+        const activeMenuFirstChild = mappedMenuList.find((e) => e.url === (router && router.asPath) || e.url === (router && router.pathname));
 
         if (activeMenuFirstChild && activeMenuFirstChild.parentKey) {
             if (activeMenuFirstChild && activeMenuFirstChild.parentKey) {
@@ -600,8 +630,7 @@ const Layout = (props) => {
             let activeMenuSecondChild = null;
 
             for (let i = 0; i < mappedMenuList.length; i += 1) {
-                if (mappedMenuList[i].url.includes(removeLastPathOnUrl((router && router.asPath))
-                || removeLastPathOnUrl((router && router.pathname)))) {
+                if (mappedMenuList[i].url.includes(removeLastPathOnUrl(router && router.asPath) || removeLastPathOnUrl(router && router.pathname))) {
                     activeMenuSecondChild = mappedMenuList[i];
                     break;
                 }
