b7d3dfdc142f056dabb4673eacae168ca1f3dc6a
alulsirclo <fazlur.rahman@sirclo.com>
2021-11-23 10:31:47 +0700

    fix([27040196] Fix Notification Feedback)

--- core/modules/notification/pages/list/components/style.js
+++ core/modules/notification/pages/list/components/style.js
@@ -1,9 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+const font = '"Roboto", "Helvetica", "Arial", sans-serif';
+
 const useStyles = makeStyles(() => ({
     fieldInput: {
         '& .MuiInputBase-input': {
             padding: '8.5px 14px',
+        },
+    },
+    controllable: {
+        '& .MuiFormControlLabel-label': {
+            fontFamily: font,
+            fontSize: 14,
         },
     },
 }));
