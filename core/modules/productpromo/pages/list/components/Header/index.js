35fb0bc2a5f95a99760b08463ad7a0dc5b8cda46
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-12-03 15:29:15 +0700

    27071698 - add module product promo

---
+++ core/modules/productpromo/pages/list/components/Header/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable object-curly-newline */
+import React from 'react';
+import { useRouter } from 'next/router';
+import useStyles from '@modules/productpromo/pages/list/components/Header/style';
+
+const HeaderContent = (props) => {
+    const classes = useStyles();
+    const router = useRouter();
+    return (
+        <div className={classes.headerContainer}>
+            <h2 className={classes.title}>Marketplace Product Promo</h2>
+        </div>
+    );
+};
+
+export default HeaderContent;
