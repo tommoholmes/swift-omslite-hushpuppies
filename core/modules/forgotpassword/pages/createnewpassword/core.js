d169e47ef3acac21b7c0ef28c7074d03e622028d
arfan21 <arfan.jadul10@gmail.com>
2021-12-03 14:48:15 +0700

    add pages forgot password & set new password

--- core/modules/forgotpassword/pages/createnewpassword/core.js
+++ core/modules/forgotpassword/pages/createnewpassword/core.js
@@ -4,23 +4,38 @@ import { useRouter } from 'next/router';
 import Layout from '@layout';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useEffect } from 'react';
 
 const Core = (props) => {
     const { Content } = props;
     const router = useRouter();
-    const [requestResetPassword] = gqlService.requestResetPassword();
+    const { token, email } = router.query;
+    const [setNewPassword] = gqlService.setNewPassword();
+
+    useEffect(() => {
+        if (!token || !email) {
+            router.push('/login');
+        }
+    }, [token, email]);
 
     const formik = useFormik({
         initialValues: {
-            email: '',
+            token: token ?? '',
+            email: email ?? '',
+            password: '',
+            password_confirmation: '',
         },
         validationSchema: Yup.object().shape({
             email: Yup.string().email().required('Required!'),
+            token: Yup.string().required('Required!'),
+            password: Yup.string().required('Required!'),
+            password_confirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
         }),
         onSubmit: (values) => {
-            const variables = { email: values.email };
+            const variables = { input: { ...values } };
+
             window.backdropLoader(true);
-            requestResetPassword({
+            setNewPassword({
                 variables,
             })
                 .then(() => {
@@ -28,7 +43,7 @@ const Core = (props) => {
                     window.toastMessage({
                         open: true,
                         variant: 'success',
-                        text: `If there is an account associated with ${values.email} you will receive an email with a link to reset your password.`,
+                        text: 'success set new password',
                     });
                     setTimeout(() => router.push('/login'), 2000);
                 })
