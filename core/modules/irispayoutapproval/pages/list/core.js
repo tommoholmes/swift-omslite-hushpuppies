88951bc8f68282dc473b0d1f25ea96699b88ed3d
vinsensiusniko <vinsensius.niko@sirclo.com>
2021-12-15 21:16:14 +0700

    27040193 - add aclCode validation Iris Payout Approval

--- core/modules/irispayoutapproval/pages/list/core.js
+++ core/modules/irispayoutapproval/pages/list/core.js
@@ -1,5 +1,7 @@
 import Layout from '@layout';
 import gqlService from '@modules/irispayoutapproval/services/graphql';
+import aclService from '@modules/theme/services/graphql';
+import { useRouter } from 'next/router';
 
 const Core = (props) => {
     const { Content } = props;
@@ -7,6 +9,19 @@ const Core = (props) => {
     const [getVendorIrisPayoutApprovalList, { data, loading }] = gqlService.getVendorIrisPayoutApprovalList();
     const [vendorIrisPayoutApprove] = gqlService.vendorIrisPayoutApprove();
     const [vendorIrisPayoutReject] = gqlService.vendorIrisPayoutReject();
+
+    const router = useRouter();
+    const { loading: aclCheckLoading, data: aclCheckData } = aclService.isAccessAllowed({
+        acl_code: 'oms_lite_vendor_iris',
+    });
+
+    if (aclCheckLoading) {
+        return <Layout>Loading...</Layout>;
+    }
+
+    if ((aclCheckData && aclCheckData.isAccessAllowed) === false) {
+        router.push('/');
+    }
 
     const contentProps = {
         getVendorIrisPayoutApprovalList,
