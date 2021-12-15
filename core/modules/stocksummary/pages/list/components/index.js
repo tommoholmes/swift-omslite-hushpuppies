ad180466df841ea0f2800a67d17663f7ae608530
arfanjadulhaq <arfan.jadulhaq@mail.sirclo.com>
2021-12-15 15:23:34 +0700

    update page stock summary list

--- core/modules/stocksummary/pages/list/components/index.js
+++ core/modules/stocksummary/pages/list/components/index.js
@@ -1,8 +1,10 @@
 import Header from '@modules/stocksummary/pages/list/components/Header';
 import React from 'react';
 import Table from '@common_table';
+import useStyles from '@modules/stocksummary/pages/list/components/style';
 
 const StockSummaryList = (props) => {
+    const classes = useStyles();
     const { data, loading, getStockSummaryList } = props;
     const stockSummaryList = (data && data.getStockSummaryList && data.getStockSummaryList.items) || [];
     const stockSummaryTotal = (data && data.getStockSummaryList && data.getStockSummaryList.total_count) || 0;
@@ -22,8 +24,32 @@ const StockSummaryList = (props) => {
             sortable: true,
         },
         {
+            field: 'channel_name',
+            headerName: 'Channel',
+            hideable: 'true',
+            sortable: true,
+        },
+        {
             field: 'channel_stock',
-            headerName: 'Channel Stock',
+            headerName: 'Stock',
+            hideable: 'true',
+            sortable: true,
+        },
+        {
+            field: 'updated_at',
+            headerName: 'Updated At',
+            hideable: 'true',
+            sortable: true,
+        },
+        {
+            field: 'sync_status_label',
+            headerName: 'Sync Status',
+            hideable: 'true',
+            sortable: true,
+        },
+        {
+            field: 'sync_message',
+            headerName: 'Sync Message',
             hideable: 'true',
             sortable: true,
         },
@@ -53,13 +79,23 @@ const StockSummaryList = (props) => {
         },
     ];
 
+    const getStatusColor = (status) => {
+        switch (status) {
+        case 'pending':
+            return classes.statusProcessing;
+        case 'success':
+            return classes.statusSuccess;
+        case 'failed':
+            return classes.statusFailed;
+        default:
+            return classes.statusClosed;
+        }
+    };
+
     const rows = stockSummaryList.map((stockSummary) => ({
         ...stockSummary,
         id: stockSummary.entity_id,
-        channel_stock: () => (
-            // eslint-disable-next-line react/no-danger
-            <span dangerouslySetInnerHTML={{ __html: stockSummary.channel_stock }} />
-        ),
+        sync_status_label: <div className={getStatusColor(stockSummary.sync_status)}>{stockSummary.sync_status_label ?? 'Unknown'}</div>,
     }));
 
     return (
