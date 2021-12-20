137b907b23918fe8ce2a0981232199226a27e42e
alulsirclo <fazlur.rahman@sirclo.com>
2021-12-20 08:08:01 +0700

    fix([27091811] Fix Shipment Detail): storepickup buttons

--- core/modules/storepickup/pages/list/components/index.js
+++ core/modules/storepickup/pages/list/components/index.js
@@ -141,7 +141,8 @@ const StorePickupListContent = (props) => {
     ];
 
     const getIconByStatus = (status) => {
-        if (status.value === 'process_for_pack' || status.value === 'process_for_shipping') {
+        if (status.value === 'process_for_pack' || status.value === 'process_for_shipping'
+        || status.value === 'pick_in_progress') {
             if (status.label === 'Cannot Fulfill') {
                 return '/assets/img/order_status/cannotfulfill.svg';
             }
@@ -150,7 +151,7 @@ const StorePickupListContent = (props) => {
         if (status.value === 'cannot_fulfill') {
             return '/assets/img/order_status/cannotfulfill.svg';
         }
-        if (status.value === 'ready_for_pack') {
+        if (status.value === 'ready_for_pack' || status.value === 'pick_uncomplete') {
             return '/assets/img/order_status/readyforpack.svg';
         }
         if (status.value === 'ready_for_pickup') {
