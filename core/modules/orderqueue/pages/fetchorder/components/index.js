dc31b1dd411f80e4b33d7cce5d920e614cdc048b
arfanjadulhaq <arfan.jadulhaq@mail.sirclo.com>
2021-12-22 17:06:52 +0700

    add label, and last fetch order

--- core/modules/orderqueue/pages/fetchorder/components/index.js
+++ core/modules/orderqueue/pages/fetchorder/components/index.js
@@ -1,41 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
 import TextField from '@common_textfield';
 import Button from '@common_button';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Paper from '@material-ui/core/Paper';
-import Autocomplete from '@common_autocomplete';
 import { useRouter } from 'next/router';
 import clsx from 'clsx';
 import useStyles from '@modules/orderqueue/pages/fetchorder/components/style';
+import gqlService from '@modules/overridestock/services/graphql';
 
 const FetchOrderContent = (props) => {
-    const {
-        formik,
-    } = props;
+    const { formik } = props;
     const classes = useStyles();
     const router = useRouter();
+    const [getActivity, getActivityRes] = gqlService.getActivity({
+        variables: {
+            code: 'manual_fetch_orders',
+            by_session: false,
+        },
+    });
+
+    useEffect(() => {
+        getActivity();
+    }, []);
 
     return (
         <>
-            <Button
-                className={classes.btnBack}
-                onClick={() => router.push('/order/allorder')}
-                variant="contained"
-                style={{ marginRight: 16 }}
-            >
-                <ChevronLeftIcon style={{
-                    fontSize: 30,
-                    position: 'absolute',
-                    left: '50%',
-                    top: '50%',
-                    transform: 'translate(-50%, -50%)',
-                }}
+            <Button className={classes.btnBack} onClick={() => router.push('/order/allorder')} variant="contained" style={{ marginRight: 16 }}>
+                <ChevronLeftIcon
+                    style={{
+                        fontSize: 30,
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%',
+                        transform: 'translate(-50%, -50%)',
+                    }}
                 />
             </Button>
             <h2 className={classes.titleTop}>Fetch Order</h2>
+
             <Paper className={classes.container}>
                 <div className={classes.content}>
+                    <h4 className={classes.titleSmall} style={{ color: 'black' }}>
+                        Please click the button below to fetch order manually
+                    </h4>
+                    {getActivityRes.data && getActivityRes.data.getActivity && (
+                        <h4 className={classes.titleSmall}>
+                            Last Fetch Order Manually By
+                            {' '}
+                            {getActivityRes.data.getActivity?.run_by_name ?? ' ... '}
+                            {' '}
+                            at
+                            {' '}
+                            {getActivityRes.data.getActivity?.finished_at}
+                        </h4>
+                    )}
                     <div className={classes.formField}>
                         <div className={classes.divLabel}>
                             <span className={clsx(classes.label, classes.labelRequired)}>Start Date</span>
@@ -80,11 +99,7 @@ const FetchOrderContent = (props) => {
                     </div>
                 </div>
                 <div className={classes.formFieldButton}>
-                    <Button
-                        className={classes.btn}
-                        onClick={formik.handleSubmit}
-                        variant="contained"
-                    >
+                    <Button className={classes.btn} onClick={formik.handleSubmit} variant="contained">
                         Submit
                     </Button>
                 </div>
