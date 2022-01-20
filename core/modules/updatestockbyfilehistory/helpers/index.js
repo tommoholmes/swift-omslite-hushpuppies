1f5576128c68dde18a83773021128312c6045e3b
vinsensiusniko <vinsensius.niko@sirclo.com>
2022-01-20 13:31:54 +0700

    fix([27138059] delete module updatestockbyfilehoistory)

--- core/modules/updatestockbyfilehistory/helpers/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const optionsHistory = [
-    { id: 0, name: 'Push Stock to Marketplace' },
-    { id: 1, name: 'Push Stock to Marketplace (VS Qty)' },
-];
